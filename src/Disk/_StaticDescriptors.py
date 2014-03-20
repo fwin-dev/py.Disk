efe0315c16de1285bfb256925a6a8803be01f295
Jesse Cowles <jcowles@indigital.net>
2014-03-20 11:33:58 -0400

    moved static descriptors in `Lang.FuncTools` to `Disk._StaticDescriptors`

---
+++ src/Disk/_StaticDescriptors.py
@@ -0,0 +1,23 @@
+from Lang.FuncTools.Abstraction import abstractmethod
+
+class FuncDesc:
+	LOCAL = 2
+	REMOTE = 3
+
+class ArgDesc:
+	SINGLE = 0
+	MULTI = 1
+
+class Descriptor(object):
+	@classmethod
+	@abstractmethod
+	def isLocal(cls, funcName):
+		pass
+	@classmethod
+	@abstractmethod
+	def getArgTypes(cls, funcName):
+		pass
+	@classmethod
+	@abstractmethod
+	def getBuiltinFunction(cls, funcName, asStr=False):
+		pass
